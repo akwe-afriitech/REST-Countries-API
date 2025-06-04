c8a1d8120691a1b981590df002460e3774a90544
EARL-MILLEN\Millen <earlmillen7@gmail.com>
2025-06-04 17:56:57 +0100

    feat:added the country details and structure the flag details page

--- src/components/flags.jsx
+++ src/components/flags.jsx
@@ -12,12 +12,62 @@ const FlagDetails = () => {
 
   return (
     <div>
-      <h1>{country.name}</h1>
-      <img src={country.flag} alt={`${country.name} flag`} />
-      <p>Population: {country.population}</p>
-      <p>Region: {country.region}</p>
-      <p>Capital: {country.capital}</p>
-      {/* Add more details as needed */}
+      <div className="back-button">
+        <button onClick={() => window.history.back()} className="back-button">
+          <i className="fas fa-arrow-left"></i> Back
+        </button>   
+      </div>
+      <div>
+        <img src={country.flag} alt={`${country.name} flag`} />
+      </div>
+      <div>
+        <h1>{country.name}</h1>
+        <div>
+          <div>
+            <p>
+              <strong>Native Name:</strong> {country.nativeName}
+            </p>
+            <p>
+              <strong>Population:</strong> {country.population}
+            </p>
+            <p>
+              <strong>Region:</strong> {country.region}
+            </p>
+            <p>
+              <strong>Sub Region:</strong> {country.subregion}
+            </p>
+            <p>
+              <strong>Capital:</strong> {country.capital}
+            </p>
+          </div>
+          <div>
+            <p>
+              <strong>Top Level Domain:</strong>{" "}
+              {country.topLevelDomain.join(", ")}
+            </p>
+            <p>
+              <strong>Currencies:</strong>{" "}
+              {country.currencies.map((currency) => currency.name).join(", ")}
+            </p>
+            <p>
+              <strong>Languages:</strong>{" "}
+              {country.languages.map((language) => language.name).join(", ")}
+            </p>
+          </div>
+        </div>
+        <div>
+          <strong>Border Countries:</strong>
+          {country.borders && country.borders.length > 0 ? (
+            <ul>
+              {country.borders.map((border) => (
+                <li key={border}>{border}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No border countries</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
